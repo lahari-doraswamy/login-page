124cc20facb140ca430b4c9a24e09b1ee409b45d
lahari.d <lahari.d@marketsimplified.com>
2023-04-06 14:29:57 +0530

    second acticiy

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
+    private EditText Username;
+
+    private Button logins;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Username = findViewById(R.id.usernames);
+        logins = findViewById(R.id.login);
+        logins.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,MainActivity2.class);
+                intent.putExtra("lahari",Username.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+
 }
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/login/MainActivity2.java
@@ -0,0 +1,24 @@
+package com.example.login;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        textView = findViewById(R.id.login1);
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            String Username = bundle.getString("lahari");
+           // textView.setText(Username);
+        }
+    }
+}
