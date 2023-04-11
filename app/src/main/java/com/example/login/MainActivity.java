0cb287633a02eb3be5429cd2a659b593034c947a
lahari.d <lahari.d@marketsimplified.com>
2023-04-11 15:55:54 +0530

    shared preferences

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -12,18 +12,25 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 public class MainActivity extends AppCompatActivity {
     private int STORAGEPERMISSONCODE = 1;
-    private EditText Username;
+    private EditText editUsername , editPassword;
 
-    private Button logins;
+    private Button btnlogin;
+    SharedPreferences sharedPreferences;
+    private static final String SHARED_PREF_NAME = "mypref";
+    private static final String KEY_NAME ="name";
+    private static final String KEY_PASSWORD="password";
+
 
 
 
@@ -31,75 +38,64 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Username = findViewById(R.id.usernames);
-        logins = findViewById(R.id.login);
-        logins.setOnClickListener(new View.OnClickListener() {
+        editUsername = findViewById(R.id.usernames);
+        editPassword=findViewById(R.id.password);
+        btnlogin = findViewById(R.id.login);
+        sharedPreferences = getSharedPreferences(SHARED_PREF_NAME,MODE_PRIVATE);
+        String name = sharedPreferences.getString(KEY_NAME,null);
+        if(name!=null){
+            Intent intent= new Intent(MainActivity.this,MainActivity2.class);
+            startActivity(intent);
+        }
+        btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-                intent.putExtra("lahari", Username.getText().toString());
+                SharedPreferences.Editor editor= sharedPreferences.edit();
+                editor.putString(KEY_NAME,editUsername.getText().toString());
+                editor.putString(KEY_PASSWORD,editPassword.getText().toString());
+                Log.d("fgd", "onClick: ");
+                editor.apply();
+                Intent intent =new Intent(MainActivity.this,MainActivity2.class);
                 startActivity(intent);
-            }
-        });
-        Button button = findViewById(R.id.request);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (ContextCompat.checkSelfPermission(MainActivity.this,
-                        android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(MainActivity.this, "you have already granted", Toast.LENGTH_SHORT).show();
-                } else {
-                    RequestStoragePermission();
-                }
+                Toast.makeText(MainActivity.this, "LoginSuccess",Toast.LENGTH_SHORT).show();
             }
         });
 
 
     }
 
- private void RequestStoragePermission(){
+    private void RequestStoragePermission(){
         if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
-           new AlertDialog.Builder(this)
-                   .setTitle("permission needed")
-                   .setMessage("This permission is needed because of this and that")
-                   .setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                       @Override
-                       public void onClick(DialogInterface dialogInterface, int i) {
+            new AlertDialog.Builder(this)
+                    .setTitle("permission needed")
+                    .setMessage("This permission is needed because of this and that")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
 
-                           ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},STORAGEPERMISSONCODE);
-                       }
-                   })
-                   .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
-                       @Override
-                       public void onClick(DialogInterface dialogInterface, int i) {
-                           dialogInterface.dismiss();
-                       }
-                   })
-                   .create().show();
+                            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},STORAGEPERMISSONCODE);
+                        }
+                    })
+                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    })
+                    .create().show();
         }else{
             ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},STORAGEPERMISSONCODE);
         }
- }
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode == STORAGEPERMISSONCODE) {
-            if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                Toast.makeText(this,"permission Granted",Toast.LENGTH_SHORT).show();
-        }
-        else {
-            Toast.makeText(this,"permission Denied",Toast.LENGTH_SHORT).show();
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == STORAGEPERMISSONCODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this, "permission Granted", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "permission Denied", Toast.LENGTH_SHORT).show();
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
--- app/src/main/java/com/example/login/MainActivity2.java
+++ app/src/main/java/com/example/login/MainActivity2.java
@@ -6,30 +6,60 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.security.Permission;
 
 public class MainActivity2 extends AppCompatActivity {
     private TextView textView;
     Button fragment1,fragment2,fragment3;
+    TextView textView_username,textView_password;
+    Button btnLogout;
+    SharedPreferences sharedPreferences;
+    private static final String SHARED_PREF_NAME="mypref";
+    private static final String SHARED_PREF_USERNAME="name";
+    private static final String SHARED_PREF_PASs="password";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        textView_username = findViewById(R.id.usernames);
+        textView_password = findViewById(R.id.Password);
+        btnLogout = findViewById(R.id.logut);
+        sharedPreferences = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
+        String Usernames = sharedPreferences.getString(SHARED_PREF_USERNAME, null);
+        String password = sharedPreferences.getString(SHARED_PREF_PASs, null);
+        if (Usernames != null || password != null) {
+            textView_username.setText("fullname -" + Usernames);
+            textView_password.setText("password -" + password);
+        }
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.commit();
+                Toast.makeText(MainActivity2.this, "logout successfully", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
         textView = findViewById(R.id.login1);
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
             String Username = bundle.getString("lahari");
-           // textView.setText(Username);
+            // textView.setText(Username);
         }
-        fragment1=findViewById(R.id.button1);
-        fragment2=findViewById(R.id.button2);
-        fragment3=findViewById(R.id.button3);
+        fragment1 = findViewById(R.id.button1);
+        fragment2 = findViewById(R.id.button2);
+        fragment3 = findViewById(R.id.button3);
         fragment1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -48,6 +78,7 @@ public class MainActivity2 extends AppCompatActivity {
                 replaceFragment(new frag3());
             }
         });
+
     }
     private void replaceFragment(Fragment fragment){
         FragmentManager fragmentManager=getSupportFragmentManager();
